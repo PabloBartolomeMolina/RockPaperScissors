55f52ea0db46745030e264e131239cbbbb2f6219
Pablo Bartolomé <pablobart.me@gmail.com>
2024-02-18 17:49:12 +0100

    Change logic for loops of the game

--- main.py
+++ main.py
@@ -5,10 +5,20 @@ options = ["Paper", "Rock", "Scissors"]
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    while True:
+    outer_flag = True
+    while outer_flag:
         game = input("Do you want to play (y/n) ?")
         if game == "y" or game == "Y":
-            continue
+            inner_flag = True
+            while inner_flag:
+                player = input("Please, select your option to play. You can also quit by writing 'quit'.")
+                if player == "quit":
+                    inner_flag = False  # Exit the inner loop
+                else:
+                    continue
+            else:
+                outer_flag = False  # Exit the outer loop
+                print("See you soon !!")
         elif game == "n" or game == "N":
             print("See you soon !!")
             break
