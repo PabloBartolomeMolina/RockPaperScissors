88dfd9fa900614d5ef4e5410a650214ea44d746c
Pablo Bartolomé <pablobart.me@gmail.com>
2024-02-18 17:59:18 +0100

    Complete logic of the game and the possible results. Computer's game is random.

--- main.py
+++ main.py
@@ -12,10 +12,20 @@ if __name__ == '__main__':
             inner_flag = True
             while inner_flag:
                 player = input("Please, select your option to play. You can also quit by writing 'quit'.")
-                if player == "quit":
+                player = player.capitalize()
+                if player == "Quit":
                     inner_flag = False  # Exit the inner loop
+                elif player in options:
+                    computer = options[randint(0, 2)]   # Random option of play for the computer.
+                    print ("The computer selected", computer)
+                    if player == computer:
+                        print("Tie !!\n")
+                    elif (player == "Paper" and computer == "Rock") or (player == "Rock" and computer == "Scissors") or (player == "Scissors" and computer == "Paper"):
+                        print("You won !!\n")
+                    else:
+                        print("You lost !!\n")
                 else:
-                    continue
+                    print("This option is not a valid one, please introduce another one.")
             else:
                 outer_flag = False  # Exit the outer loop
                 print("See you soon !!")
