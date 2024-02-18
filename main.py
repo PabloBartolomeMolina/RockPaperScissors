e6d5fe544bbd09537e20a53443263386d1e90441
Pablo Bartolomé <pablobart.me@gmail.com>
2024-02-18 17:35:16 +0100

    Basic loop of the program

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+from random import randint
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+# List of play options
+options = ["Paper", "Rock", "Scissors"]
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    while True:
+        game = input("Do you want to play (y/n) ?")
+        if game == "y" or game == "Y":
+            continue
+        elif game == "n" or game == "N":
+            print("See you soon !!")
+            break
+        else:
+            print("Invalid answer, please limit it to characters 'y' OR 'n'...")
